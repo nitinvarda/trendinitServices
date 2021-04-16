38a5a607524c3a6e2492b062182a23dfac658877
nitin <nitinvardha09@gmail.com>
2021-04-16 18:29:39 +0530

    article adding code

--- Articles/articles.js
+++ Articles/articles.js
@@ -1,10 +1,25 @@
-import {firebaseApi} from '../firebase.js'
+import firebase, {firebaseApi,storageRef} from '../firebase.js'
+import { v4 as uuidv4 } from 'uuid';
 
 
 
 const articles = {
-    create:async()=>{
+    create:async(details)=>{
         try{
+            const articleImage = storageRef.child(`images/${details.articleImage.name}`)
+            const uploadImage = await articleImage.put(details.articleImage)
+
+            const imageUrl = await storageRef.child(`images/${details.articleImage.name}`).getDownloadURL()
+            const createArticle = await firebaseApi.articles.add({
+                title:details.title,
+                by: details.by,
+                desc:details.desc,
+                category: details.category,
+                image:imageUrl,
+                createdAt:firebase.firestore.FieldValue.serverTimestamp()
+            })
+
+            return "added successfully"
 
         }
         catch(err){
