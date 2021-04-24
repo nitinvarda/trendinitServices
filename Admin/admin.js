0f50bcca2966a2886481f1b64a00b270f6bab5c4
nitin <nitinvardha09@gmail.com>
2021-04-24 18:41:14 +0530

    final change

--- Admin/admin.js
+++ Admin/admin.js
@@ -1,11 +1,11 @@
-import firebase,{firebaseApi} from '../firebase.js'
+import firebase,{firebaseApi,auth} from '../firebase.js'
 
 
 
 const admin ={
     login:async(email,password)=>{
         try{
-            const signInAdmin = await firebase.auth().signInWithEmailAndPassword(email,password)
+            const signInAdmin = await auth().signInWithEmailAndPassword(email,password)
             const result = signInAdmin.user
            
 
@@ -22,7 +22,7 @@ const admin ={
     },
     register:async(email,password,username)=>{
         try{
-            const createAdmin = await firebase.auth().createUserWithEmailAndPassword(email,password)
+            const createAdmin = await auth().createUserWithEmailAndPassword(email,password)
             const result = createAdmin.user
 
             const updateInfo = result.updateProfile({
