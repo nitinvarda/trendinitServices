135f8a7113fe5363a99dd2dda905e799bc6c5d35
nitin <nitinvardha09@gmail.com>
2021-04-24 18:40:08 +0530

    final changes

--- firebase.js
+++ firebase.js
@@ -1,30 +1,61 @@
 import firebase from 'firebase'
 import '@firebase/storage'
-// import dotenv from 'dotenv'
-
-// dotenv.config()
-
-const firebaseConfig = {
-    apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    messagingSenderId:process.env.REACT_APP_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_APP_ID
-};
-
-console.log(firebaseConfig)
+import {
+    REACT_APP_API_KEY,
+    REACT_APP_AUTH_DOMAIN,
+    REACT_APP_PROJECT_ID,
+    REACT_APP_STORAGE_BUCKET,
+    REACT_APP_MESSAGING_SENDER_ID,
+    REACT_APP_APP_ID
+} from 'dotenv'
 
 
-console.log("this is submoudle test")
 
+const getFirebaseConfig = () =>{
+
+
+if(!process.env.REACT_APP_API_KEY){
+    
+        const config = {
+            apiKey:REACT_APP_API_KEY,
+            authDomain:REACT_APP_AUTH_DOMAIN,
+            projectId:REACT_APP_PROJECT_ID,
+            storageBucket:REACT_APP_STORAGE_BUCKET,
+            messagingSenderId:REACT_APP_MESSAGING_SENDER_ID,
+            appId:REACT_APP_APP_ID,
+            
+        }
+        return config
+  
+   
+
+
+}
+else{
+    const config = {
+        apiKey:process.env.REACT_APP_API_KEY,
+        authDomain:process.env.REACT_APP_AUTH_DOMAIN,
+        projectId:process.env.REACT_APP_PROJECT_ID,
+        storageBucket:process.env.REACT_APP_STORAGE_BUCKET,
+        messagingSenderId:process.env.REACT_APP_MESSAGING_SENDER_ID,
+        appId:process.env.REACT_APP_APP_ID
+
+    }
+    return config
+}
+
+}
+
+
+const firebaseConfig = getFirebaseConfig()
 firebase.initializeApp(firebaseConfig);
 
 
 
 const firestore = firebase.firestore()
+const storage = firebase.storage().ref()
+const auth =()=> firebase.auth()
 
-const storageRef = firebase.storage().ref()
 
 
 
@@ -35,4 +66,4 @@ const firebaseApi = {
 }
 
 export default firebase
-export {firebaseApi,storageRef}
+export {firebaseApi,storage,auth}
